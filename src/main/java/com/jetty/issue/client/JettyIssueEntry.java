7858a7db58bd407323bb01f5926b9ce8f477444f
BenDol <dolb90@gmail.com>
2016-05-18 17:19:30 +1200

    null.$_nullMethod() bug.

--- src/main/java/com/jetty/issue/client/JettyIssueEntry.java
+++ src/main/java/com/jetty/issue/client/JettyIssueEntry.java
@@ -1,16 +1,24 @@
 package com.jetty.issue.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.ScriptInjector;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
+import jsinterop.annotations.JsProperty;
+import jsinterop.annotations.JsType;
 
 public class JettyIssueEntry implements EntryPoint {
 
+    @JsType(name = "Window", isNative = true)
+    public interface HTMLWindow {
+        @JsProperty String getName();
+        @JsProperty void setName(String name);
+    }
+
     @Override
     public void onModuleLoad() {
-        GWT.setUncaughtExceptionHandler(e -> GWT.log(e.getMessage(), e));
-
-        RootPanel.get().add(new Label("Loaded!"));
+        HTMLWindow window = (HTMLWindow) ScriptInjector.TOP_WINDOW;
+        window.setName("Window Name");
+        RootPanel.get().add(new Label(window.getName()));
     }
 }
